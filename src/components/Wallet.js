94dc23a814570e7095e1da922849ea4fe3984242
Karl Krasnowsky <karlkras@yahoo.com>
2018-08-06 03:16:28 +0000

    completion of lecture-30

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,11 +3,28 @@ import { connect } from 'react-redux';
 
 
 export class Wallet extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = { 
+            balance: undefined 
+        };
+    }
+    
+    updateBalance = evt => {
+        const balance = parseInt(evt.target.value, 10);
+        this.setState({balance});
+    }
     
     render() {
         return (
             <div>
                 <h3 className={'balance'} >Wallet balance: {this.props.balance}</h3>
+                <br/>
+                <input
+                    className={'input-wallet'}
+                    onChange={this.updateBalance}
+                />
             </div>
         );
     };
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -14,4 +14,21 @@ describe('Wallet', () => {
     it('displays the balance from props', () => {
         expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
     });
+    
+    it('creates an input to depost into or withdrawl from the balance', () => {
+        expect(wallet.find('.input-wallet').exists()).toBe(true);
+    });
+    
+    describe('when the user types into the wallet input', () => {
+        const userBalance = '25';
+        
+        beforeEach(() => {
+            wallet.find('.input-wallet')
+                .simulate('change', { target: { value: userBalance } });
+        });
+        
+        it('updates the local wallet balance in the `state` and converts it into a number', () => {
+            expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+        });
+    });
 });
